f5e06a75dddcd05c85814f335d4d0312818d0057
Artur Pelo <artur.pelo@gmail.com>
2024-10-22 11:36:00 +0200

    Zdefiniowane 2 metody - zorientowana na klasę i zorientowana na metodę

--- src/Listy/Main.java
+++ src/Listy/Main.java
@@ -7,22 +7,34 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        LinkedList<Student> listaStudentow = new LinkedList<>();
+        LinkedList<Student> studentList1 = new LinkedList<>();
+        ArrayList<Student> studentList2 = new ArrayList<>();
 
-        Student student = new Student("Jan", "Sowa");
-        listaStudentow.add(student);
+        studentList1.add(new Student("Jan", "Sowa"));
+        studentList1.add(new Student("Alicja","Kot"));
+        studentList1.add(new Student("Tomasz","Nowak"));
 
-        listaStudentow.add(new Student("Alicja","Kot"));
-        listaStudentow.add(new Student("Tomasz","Nowak"));
+        studentList2.add(new Student("Marek", "Kowal"));
+        studentList2.add(new Student("Anna","Kowalska"));
+        studentList2.add(new Student("Tomasz","Nowak"));
 
-        showList(listaStudentow);
+        showList(studentList1);
+       // showList(studentList2);       //niedozwolone bo metoda przyjmyje LinkedList
 
-
+        showElements(studentList1);     //dozwolone - zobacz metodę, (interface List)
+        showElements(studentList2);     //dozwolone - zobacz metodę, (interface List)
     }
 
-    public static void showList(LinkedList<Student> lista){
+    public static void showList(LinkedList<Student> lista){ //tylko dla LinkedList
      for(Student element: lista){
          System.out.println(element);
      }
     }
+    public static void showElements(List<Student> lista){ //dla LinkedList i ArrayList
+        for(Student element: lista){
+            System.out.println(element);
+        }
+
+    }
+
 }
